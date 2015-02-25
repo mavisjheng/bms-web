9614c95e803051bcf35e93192a1a924585fb00db
mavisjheng <erjhen@gmail.com>
2015-02-25 19:06:34 +0800

    add network.html and ACE font and css files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// breadcrumb
     $('.service-pills ul li a').click(function() {
         var currentTabText = $(this).text();
         $('.breadcrumb li.active').text(currentTabText);
@@ -6,42 +7,36 @@ $(document).ready(function() {
 
     // subscribe notification
     $('.subscribe-btn').click(function(){
-        $('.subscribe').animate({right: "0px"}, 200);
-        $('.left-part, .mid-part').animate({right: "300px"}, 200);
+        $('.subscribe').animate({right: "0px"});
     });
     $('.subscribe-close').click(function(){
-        $('.subscribe').animate({right: "-300px"}, 200);
-        $('.left-part, .mid-part').animate({right: "0px"}, 200);
+        $('.subscribe').animate({right: "-300px"});
     });
 
 	// my account
     $('.account-btn').click(function(){
-        $('.account').animate({right: "0px"}, 200);
-        $('.left-part, .mid-part').animate({right: "300px"}, 200);
+        $('.account').animate({right: "0px"});
     });
     $('.account-close').click(function(){
-        $('.account').animate({right: "-300px"}, 200);
-        $('.left-part, .mid-part').animate({right: "0px"}, 200);
+        $('.account').animate({right: "-300px"});
     });
 
     // contact us
     $('.contact-btn').click(function(){
-        $('.contact').animate({right: "0px"}, 200);
-        $('.left-part, .mid-part').animate({right: "300px"}, 200);
+        $('.contact').animate({right: "0px"});
     });
     $('.contact-close').click(function(){
-        $('.contact').animate({right: "-300px"}, 200);
-        $('.left-part, .mid-part').animate({right: "0px"}, 200);
+        $('.contact').animate({right: "-300px"});
     });
 
     // log in
     $('.login-btn').click(function(){
-        $('.login').animate({right: "0px"}, 200);
-        $('.left-part, .mid-part').animate({right: "300px"}, 200);
+        $('.login').animate({right: "0px"});
+        //$('.left-part, .mid-part').animate({right: "300px"}, 200);
     });
     $('.login-close').click(function(){
-        $('.login').animate({right: "-300px"}, 200);
-        $('.left-part, .mid-part').animate({right: "0px"}, 200);
+        $('.login').animate({right: "-300px"});
+        //$('.left-part, .mid-part').animate({right: "0px"}, 200);
     });
 
 });
